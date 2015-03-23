97f39b45edb83bcd81896f00b6f8c9b5af5c4a2d
karla <karlacn@outlook.com>
2015-03-22 23:25:12 -0700

    Complete functionality of steps 1-5, as well as generated tidy.txt resulting from the last step

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,3 @@
-#You should create one R script called run_analysis.R that does the following. 
-
-#1. Merges the training and the test sets to create one data set.
-#2. Extracts only the measurements on the mean and standard deviation for each 
-#   measurement. 
-#3. Uses descriptive activity names to name the activities in the data set
-#4. Appropriately labels the data set with descriptive variable names. 
-#5. From the data set in step 4, creates a second, independent tidy data set 
-#   with the average of each variable for each activity and each subject.
 
 # It is assumed that a folder UCI HAR Dataset is located on the working 
 # directory.  Inside there should be 2 folders: test, and train. Before 
@@ -19,6 +10,25 @@ if(!file.exists(".//UCI HAR Dataset"))
 library(data.table)
 library(dplyr)
 library(tidyr)
+
+# ===Step 4: Appropriately label the data set with descriptive variable names ==
+#
+# In preparation for this we need to have descriptive names for the columns in
+# the tidy table we will have.  we know we want to use columns "subject", "activity",
+# and all of the columns as described on features.txt for which we need to
+# gather these column names to do a proper binding when the time comes.
+#
+# The document features.txt has 2 columns, the first one is nothing but a row
+# number or index, the second one is the actual name for that feature, which is
+# the only one we are interested in, so we take the table and get only the second
+# column.
+# =============================================================================
+measurement_names <- read.table(".//UCI HAR Dataset//features.txt", 
+                                stringsAsFactors = FALSE)[,2]
+print(
+  sprintf("DBG: measurement_names calculated with dimensions: [%s]", 
+          length(measurement_names)))
+
 
 # ===Step 1: Merge training and test sets to create one data set ==============
 #
@@ -34,7 +44,8 @@ library(tidyr)
 # =============================================================================
 merged_train  <- read.table(".//UCI HAR Dataset//train//subject_train.txt", col.names = c("subject")) %>% 
   bind_cols (read.table(".//UCI HAR Dataset//train//y_train.txt", col.names = c("activity"))) %>%
-  bind_cols (read.table(".//UCI HAR Dataset//train//x_train.txt")) %>% tbl_df
+  bind_cols (read.table(".//UCI HAR Dataset//train//x_train.txt", col.names = measurement_names)) %>%
+  tbl_df
 
 print(
   sprintf("DBG: merged_train calculated with dimensions: [%s, %s]", 
@@ -43,15 +54,15 @@ print(
 
 merged_test  <- read.table(".//UCI HAR Dataset//test//subject_test.txt", col.names = c("subject")) %>% 
   bind_cols (read.table(".//UCI HAR Dataset//test//y_test.txt", col.names = c("activity"))) %>%
-  bind_cols (read.table(".//UCI HAR Dataset//test//x_test.txt")) %>% tbl_df
+  bind_cols (read.table(".//UCI HAR Dataset//test//x_test.txt", col.names = measurement_names)) %>%
+  tbl_df
 
 print(
   sprintf("DBG: merged_test calculated with dimensions: [%s, %s]", 
           dim(merged_test)[1], 
           dim(merged_test)[2]))
 
-merged_sets <- bind_rows(merged_train, merged_test) %>%
-  tbl_df
+merged_sets <- bind_rows(merged_train, merged_test)
 rm("merged_train", "merged_test") #clean-up memory since we don't need these variables anymore
 print(
   sprintf("DBG: merged_sets calculated with dimensions: [%s, %s]", 
@@ -69,7 +80,54 @@ print(
 #
 # =============================================================================
 
-features_table <- read.table(".//UCI HAR Dataset//features.txt", col.names = c("id", "measures"))
-  
-#  names(merged_sets) <- c("subject", "activity", as.character(features_table$measures))
-#rm("features_table")
+rm("measurement_names") #cleanup variables that we won't be using anymore
+merged_sets <- merged_sets %>%
+  select(subject, activity, contains("mean"), contains("std"))
+print(
+  sprintf("DBG: merged_sets with only mean and std with dimensions: [%s, %s]", 
+          dim(merged_sets)[1], 
+          dim(merged_sets)[2]))
+
+
+# ===Step 3: Use descriptive activity names in the data set ===================
+#
+# The activity column has numbers 1-6 that map to the different activities that
+# were performed.  There is a file activity_labels.txt that has the mapping of
+# number to description.  We need to create a reference table by reading the txt
+# file
+# In this file there's a table with 2 columns, the first one is an index and the
+# second one is the corresponding string for that index.
+#
+# =============================================================================
+
+activities <- read.table(".//UCI HAR Dataset//activity_labels.txt", 
+                         stringsAsFactors = FALSE,
+                         col.names = c("value", "name"))
+
+# helper function to find the name corresponding to the activity
+findVerboseActivity<-function(theList) {
+  result = c()
+  for(currValue in theList)
+    result <- c(result, activities[activities$value == currValue,"name"])
+  unlist(result)
+}
+
+merged_sets <- merged_sets %>%
+  mutate(activity = findVerboseActivity(activity))
+rm("activities") # data cleanup
+
+print(
+  sprintf("DBG: merged_sets with descriptive activity names: [%s, %s]", 
+          dim(merged_sets)[1], 
+          dim(merged_sets)[2]))
+
+# === Step 5: From the data set in step 4, create a second tidy dataset with ==
+# === the average of each variable for each activity and each subject. ========
+
+averaged_set <- merged_sets %>%
+  group_by(activity, subject) %>%
+  summarise_each(funs(mean))
+print(
+  sprintf("DBG: averaged_set with dimensions: [%s, %s]", 
+          dim(averaged_set)[1], 
+          dim(averaged_set)[2]))
